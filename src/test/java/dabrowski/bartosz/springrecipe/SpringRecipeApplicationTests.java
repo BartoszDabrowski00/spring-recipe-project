32db168dd786f444b0c09ee7b4448cd81a03fa32
Bartek <bartosz.dabrowski2000@gmail.com>
2021-07-31 17:06:55 +0200

    initial project

---
+++ src/test/java/dabrowski/bartosz/springrecipe/SpringRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package dabrowski.bartosz.springrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
