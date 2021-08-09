246d05b709009c261d237dbbb34e8193c37944d8
Bartek <bartosz.dabrowski2000@gmail.com>
2021-08-09 10:59:32 +0200

    added recipe, ingredient,  notes entities with relationships

---
+++ src/main/java/dabrowski/bartosz/springrecipe/domain/Ingredient.java
@@ -0,0 +1,23 @@
+package dabrowski.bartosz.springrecipe.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+}
---
+++ src/main/java/dabrowski/bartosz/springrecipe/domain/Notes.java
@@ -0,0 +1,22 @@
+package dabrowski.bartosz.springrecipe.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+}
