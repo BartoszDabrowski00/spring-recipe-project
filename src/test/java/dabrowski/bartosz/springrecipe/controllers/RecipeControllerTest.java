2a3814c4b6eecdb408790d10d30cd6633ea90227
Bartek <bartosz.dabrowski2000@gmail.com>
2021-08-29 19:26:30 +0200

    fixed bug with testPostNewRecipeForm

--- src/test/java/dabrowski/bartosz/springrecipe/controllers/RecipeControllerTest.java
+++ src/test/java/dabrowski/bartosz/springrecipe/controllers/RecipeControllerTest.java
@@ -71,7 +71,8 @@ class RecipeControllerTest {
                 .perform(post("/recipe")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .param("id", "")
-                        .param("description", "some string"))
+                        .param("description", "some string")
+                        .param("directions", "some string"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
     }
