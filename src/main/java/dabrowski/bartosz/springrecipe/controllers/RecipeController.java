595cb1898b610a027d9f63003f76a2cb20a9b808
Bartek <bartosz.dabrowski2000@gmail.com>
2021-08-29 16:25:21 +0200

    added validation to recipe command

--- src/main/java/dabrowski/bartosz/springrecipe/controllers/RecipeController.java
+++ src/main/java/dabrowski/bartosz/springrecipe/controllers/RecipeController.java
@@ -8,8 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @AllArgsConstructor
 @Controller
@@ -33,9 +36,12 @@ public class RecipeController {
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult result){
+        if(result.hasErrors()){
+            result.getAllErrors().forEach(err->log.debug(err.toString()));
+            return "/recipe/new";
+        }
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
-
         return "redirect:/recipe/"+savedCommand.getId()+"/show";
     }
 
